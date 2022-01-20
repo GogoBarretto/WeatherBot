6debed04e21028b1111fa8f561e96d0170ae117e
GogoBarretto <73837571+GogoBarretto@users.noreply.github.com>
2022-01-20 19:05:20 +0300

    Add files via upload

---
+++ Functionality.cs
@@ -0,0 +1,75 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+using System.Net;
+
+
+namespace pvcWeatherBot
+{
+    public class Functionality
+    {
+        public static string GetWeather(string msgTxt)
+        {
+
+
+            {
+                try
+                {
+                    //Сделать смену языка
+                    WeatherAPIToken token = new WeatherAPIToken();
+
+                    string url = $"http://api.openweathermap.org/data/2.5/weather?q={msgTxt}&units=metric&appid={token.token}&lang=ru";
+                    HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+                    HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+
+                    string response;
+
+                    using (StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream()))
+                    {
+                        response = streamReader.ReadToEnd();
+                    }
+
+                    return response;
+                }
+                catch
+                {
+                    return "Ошибка: Такой город не найден!";
+                }
+            }
+        }
+        public static string MessageBuilder(string response)
+        {
+            WeatherResponse weatherJson = JsonConvert.DeserializeObject<WeatherResponse>(response);
+
+            string description = "";
+            foreach (var item in weatherJson.weather)
+            {
+                description = item.description;
+            }
+
+            string weatherMessage =
+            "Город: " + weatherJson.Name +
+            "\nОсадки: " + description +
+            "\nТемпература: " + weatherJson.Main.Temp + " °C" +
+            "\nЧувствуется как: " + weatherJson.Main.Feels_like + " °C" +
+            "\nДавление: " + weatherJson.Main.Pressure + " мм рт.ст." +
+            "\nВлажность: " + weatherJson.Main.Humidity + " %" +
+            "\nСкорость ветра: " + weatherJson.Wind.Speed + " м/c";
+
+            return weatherMessage;
+        }
+        public static string ResponseBuilder(string MsgText)
+        {
+            string responseByCity = GetWeather(MsgText);
+            string resultMessage;
+            if (responseByCity != "Ошибка: Такой город не найден!")
+            {
+                resultMessage = MessageBuilder(responseByCity);
+            }
+            else
+            {
+                resultMessage = responseByCity;
+            }
+            return resultMessage;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,85 +1,15 @@
 ﻿using System;
-using Telegram.Bot;
-using System.Net;
 using System.Threading.Tasks;
-using System.Linq;
-using System.IO;
 using System.Threading;
+using Telegram.Bot;
 using Telegram.Bot.Exceptions;
 using Telegram.Bot.Extensions.Polling;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
-using Newtonsoft.Json;
+
 
 namespace pvcWeatherBot
 {
-    public class Functionality
-    {
-        public static string GetWeather(string msgTxt)
-        {
-
-            
-            {
-                try
-                {
-                    //Сделать смену языка
-                    WeatherAPIToken token = new WeatherAPIToken();
-
-                    string url = $"http://api.openweathermap.org/data/2.5/weather?q={msgTxt}&units=metric&appid={token.token}&lang=ru";
-                    HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
-                    HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
-
-                    string response;
-
-                    using (StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream()))
-                    {
-                        response = streamReader.ReadToEnd();
-                    }
-                   
-                    return response;   
-                }
-                catch
-                {
-                    return "Ошибка: Такой город не найден!";
-                }
-            }
-        }
-        public static string MessageBuilder(string response)
-        {
-            WeatherResponse weatherJson = JsonConvert.DeserializeObject<WeatherResponse>(response);
-
-            string description = "";
-            foreach(var item in weatherJson.weather)
-            {
-                description = item.description;
-            }
-
-            string weatherMessage =
-            "Город: " + weatherJson.Name +
-            "\nОсадки: " + description +
-            "\nТемпература: " + weatherJson.Main.Temp + " °C" +
-            "\nЧувствуется как: " + weatherJson.Main.Feels_like + " °C" +
-            "\nДавление: " + weatherJson.Main.Pressure + " мм рт.ст." +
-            "\nВлажность: " + weatherJson.Main.Humidity + " %" +
-            "\nСкорость ветра: " + weatherJson.Wind.Speed + " м/c";
-        
-            return weatherMessage;
-        }
-        public static string ResponseBuilder(string MsgText)
-        {
-            string responseByCity = GetWeather(MsgText);
-            string resultMessage;
-            if (responseByCity != "Ошибка: Такой город не найден!") {
-                resultMessage = MessageBuilder(responseByCity);
-            }
-            else
-            {
-                resultMessage = responseByCity;
-            }
-            return resultMessage;
-        }
-    }
-
     class Program
     {
 
@@ -125,8 +55,7 @@ namespace pvcWeatherBot
 
                 Message sentMessage = await botClient.SendTextMessageAsync(
                     chatId: chatId,
-                    text:  $"{Functionality.ResponseBuilder(messageText)}\n",//Создание ответа на запрос погоды 
-                    
+                    text:  $"{Functionality.ResponseBuilder(messageText)}\n",//Создание ответа на запрос погоды    
                     cancellationToken: cancellationToken);
             }
             
